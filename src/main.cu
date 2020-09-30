be006811a8f70a1f0e1efe794719eb6022857cfb
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-09-30 14:21:23 -0400

    Computing a timestep crashes my computer.

--- src/main.cu
+++ src/main.cu
@@ -442,8 +442,9 @@ int main(void) {
     }
 
     // Starting actual computation
-    cudaDeviceSynchronize();
-    compute_dg_time_derivative<<<elements_numBlocks, elements_blockSize>>>(N_elements, elements, weights, derivative_matrices_hat, lagrange_interpolant_left, lagrange_interpolant_right);
+    //cudaDeviceSynchronize();
+    // This one right here officer
+    //compute_dg_time_derivative<<<elements_numBlocks, elements_blockSize>>>(N_elements, elements, weights, derivative_matrices_hat, lagrange_interpolant_left, lagrange_interpolant_right);
     
     // Wait for GPU to finish before copying to host
     cudaDeviceSynchronize();
