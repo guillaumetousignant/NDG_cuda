c2b98a7f200351ae2d72f462536a6f5a8d56920c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-11-26 12:30:14 -0500

    Outputs more stuff

--- src/main.cu
+++ src/main.cu
@@ -578,6 +578,7 @@ void build_elements(int N_elements, int N, Element_t* elements, float x_min, flo
 
 __device__
 float g(float x) {
+    //return (x < -0.2f || x > 0.2f) ? 0.2f : 0.8f;
     return -std::sin(pi * x);
 }
 
@@ -1020,29 +1021,47 @@ public:
 int main(void) {
     const int N_elements = 4;
     const int N_max = 8;
-    const int initial_N = N_max;
-    const int N_interpolation_points = 1000;
-    const float delta_t = 0.00015f;
-    std::vector<float> output_times{0.1f, 0.2f, 0.3f, 0.4f, 0.5f};
-    
-    NDG_t NDG(N_max, N_interpolation_points);
-    Mesh_t Mesh(N_elements, initial_N, -1.0f, 1.0f);
-    Mesh.set_initial_conditions(NDG.nodes_);
+    const float x[2] = {-1.0f, 1.0f};
+    const float CFL = 0.5f;
+    const float u_max = 1.0f;
 
-    // Starting actual computation
+    const int initial_N = N_max;
+    const int N_interpolation_points = N_max * 8;
+    const float delta_x = (x[1] - x[0])/N_elements;
+    const float delta_t = CFL * delta_x/u_max;
+    std::vector<float> output_times{0.1f, 0.2f, 0.3f, 0.4f, 0.5f};
+
+    std::cout << "CFL is: " << CFL << std::endl;
+    std::cout << "Delta t is: " << delta_t << std::endl;
+
+    // Initialisation
+    auto t_start_init = std::chrono::high_resolution_clock::now();
+
+    NDG_t NDG(N_max, N_interpolation_points);
+    Mesh_t Mesh(N_elements, initial_N, x[0], x[1]);
+    Mesh.set_initial_conditions(NDG.nodes_);
     cudaDeviceSynchronize();
+
+    auto t_end_init = std::chrono::high_resolution_clock::now();
+    std::cout << "GPU initialisation time: " 
+            << std::chrono::duration<double, std::milli>(t_end_init - t_start_init).count()/1000.0 
+            << "s." << std::endl;
+
+    // Computation
     auto t_start = std::chrono::high_resolution_clock::now();
+
     Mesh.solve(delta_t, output_times, NDG);
     // Wait for GPU to finish before copying to host
     cudaDeviceSynchronize();
-    auto t_end = std::chrono::high_resolution_clock::now();
 
+    auto t_end = std::chrono::high_resolution_clock::now();
     std::cout << "GPU computation time: " 
-            << std::chrono::duration<double, std::milli>(t_end-t_start).count()/1000.0 
+            << std::chrono::duration<double, std::milli>(t_end - t_start).count()/1000.0 
             << "s." << std::endl;
 
+    // Printing debug values
     //NDG.print();
-    Mesh.print();
+    //Mesh.print();
     
     return 0;
 }
