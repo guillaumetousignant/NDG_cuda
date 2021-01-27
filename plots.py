2aa6c08484af8d6c0678a9f42145b77be3f03656
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-01-27 16:13:58 -0500

    This is all clearer

--- plots.py
+++ plots.py
@@ -74,17 +74,17 @@ for filename in filenames_element:
             error_arrays[-1][i] = float(numbers[6])
 
 # Plotting 
-vline_color = [0.5, 0.5, 0.5]
+N_timesteps = len(filenames)
 vline_alpha = 0.2
 vline_linestyle = '--'
 color_map = plt.get_cmap("rainbow")
 
 ux_fig, ux_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    ux_ax.plot(x_arrays[i], ux_arrays[i], color=color_map(1.0 * i/len(filenames)), label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    ux_ax.plot(x_arrays[i], ux_arrays[i], color=color_map(i/N_timesteps), label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        ux_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    ux_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        ux_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    ux_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 ux_ax.grid()
 ux_ax.set_ylabel('$U_x$ [$m/s$]')
@@ -93,11 +93,11 @@ ux_ax.set_title("$U_x$ along x")
 ux_ax.legend(loc='best')
 
 ux_prime_fig, ux_prime_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    ux_prime_ax.plot(x_arrays[i], ux_prime_arrays[i], color=color_map(1.0 * i/len(filenames)), label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    ux_prime_ax.plot(x_arrays[i], ux_prime_arrays[i], color=color_map(i/N_timesteps), label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        ux_prime_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    ux_prime_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        ux_prime_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    ux_prime_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 ux_prime_ax.grid()
 ux_prime_ax.set_ylabel('$U_x,prime$ [$1/s$]')
@@ -106,11 +106,11 @@ ux_prime_ax.set_title("$U_x,prime$ along x")
 ux_prime_ax.legend(loc='best')
 
 intermediate_fig, intermediate_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    intermediate_ax.semilogy(x_arrays[i], intermediate_arrays[i], color=color_map(1.0 * i/len(filenames)), label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    intermediate_ax.semilogy(x_arrays[i], intermediate_arrays[i], color=color_map(i/N_timesteps), label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        intermediate_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    intermediate_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        intermediate_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    intermediate_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 intermediate_ax.grid()
 intermediate_ax.set_ylabel('Intermediate [?]')
@@ -119,11 +119,11 @@ intermediate_ax.set_title("Intermediate along x")
 intermediate_ax.legend(loc='best')
 
 sigma_fig, sigma_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    sigma_ax.plot(x_element_arrays[i], sigma_arrays[i], color=color_map(1.0 * i/len(filenames)), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    sigma_ax.plot(x_element_arrays[i], sigma_arrays[i], color=color_map(i/N_timesteps), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        sigma_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    sigma_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        sigma_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    sigma_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 sigma_ax.grid()
 sigma_ax.set_ylabel('$sigma$ [?]')
@@ -132,11 +132,11 @@ sigma_ax.set_title("$sigma$ along x")
 sigma_ax.legend(loc='best')
 
 refine_fig, refine_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    refine_ax.plot(x_element_arrays[i], refine_arrays[i], color=color_map(1.0 * i/len(filenames)), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    refine_ax.plot(x_element_arrays[i], refine_arrays[i], color=color_map(i/N_timesteps), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        refine_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    refine_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        refine_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    refine_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 refine_ax.grid()
 refine_ax.set_ylabel('Refine flag [bool]')
@@ -145,11 +145,11 @@ refine_ax.set_title("Refine flag along x")
 refine_ax.legend(loc='best')
 
 coarsen_fig, coarsen_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    coarsen_ax.plot(x_element_arrays[i], coarsen_arrays[i], color=color_map(1.0 * i/len(filenames)), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    coarsen_ax.plot(x_element_arrays[i], coarsen_arrays[i], color=color_map(i/N_timesteps), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        coarsen_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    coarsen_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        coarsen_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    coarsen_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 coarsen_ax.grid()
 coarsen_ax.set_ylabel('Coarsen flag [bool]')
@@ -158,11 +158,11 @@ coarsen_ax.set_title("Coarsen flag along x")
 coarsen_ax.legend(loc='best')
 
 error_fig, error_ax = plt.subplots(1, 1)
-for i in range(len(filenames)):
-    error_ax.semilogy(x_element_arrays[i], error_arrays[i], color=color_map(1.0 * i/len(filenames)), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
+for i in range(N_timesteps):
+    error_ax.semilogy(x_element_arrays[i], error_arrays[i], color=color_map(i/N_timesteps), marker='+', markeredgewidth=2, markersize=16, label=f"t = {times[i]} s")
     for x_L in x_L_arrays[i]:
-        error_ax.axvline(x=x_L, color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
-    error_ax.axvline(x=x_R_arrays[i][-1], color=color_map(1.0 * i/len(filenames)), alpha=vline_alpha, linestyle=vline_linestyle)
+        error_ax.axvline(x=x_L, color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
+    error_ax.axvline(x=x_R_arrays[i][-1], color=color_map(i/N_timesteps), alpha=vline_alpha, linestyle=vline_linestyle)
 
 error_ax.grid()
 error_ax.set_ylabel('Error estimation [?]')
