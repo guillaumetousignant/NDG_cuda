baa8ee606927a8b86664eb2671f3524a805cf31f
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-01-27 10:08:35 -0500

    this should reduce delta_t

--- include/Mesh_t.cuh
+++ include/Mesh_t.cuh
@@ -6,6 +6,7 @@
 #include "NDG_t.cuh"
 #include "float_types.h"
 #include <vector>
+#include <limits>
 
 class Mesh_t {
 public:
@@ -60,50 +61,54 @@ namespace SEM {
     // From https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
     template <unsigned int blockSize>
     __device__ 
-    void warp_reduce_velocity(volatile deviceFloat *sdata, unsigned int tid) {
-        if (blockSize >= 64) sdata[tid] = max(sdata[tid], sdata[tid + 32]);
-        if (blockSize >= 32) sdata[tid] = max(sdata[tid], sdata[tid + 16]);
-        if (blockSize >= 16) sdata[tid] = max(sdata[tid], sdata[tid + 8]);
-        if (blockSize >= 8) sdata[tid] = max(sdata[tid], sdata[tid + 4]);
-        if (blockSize >= 4) sdata[tid] = max(sdata[tid], sdata[tid + 2]);
-        if (blockSize >= 2) sdata[tid] = max(sdata[tid], sdata[tid + 1]);
+    void warp_reduce_delta_t(volatile deviceFloat *sdata, unsigned int tid) {
+        if (blockSize >= 64) sdata[tid] = min(sdata[tid], sdata[tid + 32]);
+        if (blockSize >= 32) sdata[tid] = min(sdata[tid], sdata[tid + 16]);
+        if (blockSize >= 16) sdata[tid] = min(sdata[tid], sdata[tid + 8]);
+        if (blockSize >= 8) sdata[tid] = min(sdata[tid], sdata[tid + 4]);
+        if (blockSize >= 4) sdata[tid] = min(sdata[tid], sdata[tid + 2]);
+        if (blockSize >= 2) sdata[tid] = min(sdata[tid], sdata[tid + 1]);
     }
 
     // From https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
     template <unsigned int blockSize>
     __global__ 
-    void reduce_velocity(size_t N_elements, const Element_t* elements, deviceFloat *g_odata) {
+    void reduce_delta_t(deviceFloat CFL, size_t N_elements, const Element_t* elements, deviceFloat *g_odata) {
         __shared__ deviceFloat sdata[blockSize];
         unsigned int tid = threadIdx.x;
         size_t i = blockIdx.x*(blockSize*2) + tid;
         unsigned int gridSize = blockSize*2*gridDim.x;
-        sdata[tid] = 0.0;
+        sdata[tid] = std::numeric_limits<deviceFloat>::infinity();
 
         while (i < N_elements) { 
             deviceFloat phi_max = 0.0;
             for (int j = 0; j <= elements[i].N_; ++j) {
                 phi_max = max(phi_max, abs(elements[i].phi_[j]));
             }
+            deviceFloat delta_t = CFL * elements[i].delta_x_/(phi_max * elements[i].N_ * elements[i].N_);
+ 
             if (i+blockSize < N_elements) {
+                phi_max = 0.0;
                 for (int j = 0; j <= elements[i+blockSize].N_; ++j) {
                     phi_max = max(phi_max, abs(elements[i+blockSize].phi_[j]));
                 }
+                delta_t = min(delta_t, CFL * elements[i+blockSize].delta_x_/(phi_max * elements[i+blockSize].N_ * elements[i+blockSize].N_));
             }
 
-            sdata[tid] = max(sdata[tid], phi_max); 
+            sdata[tid] = min(sdata[tid], phi_max); 
             i += gridSize; 
         }
         __syncthreads();
 
-        if (blockSize >= 8192) { if (tid < 4096) { sdata[tid] = max(sdata[tid], sdata[tid + 4096]); } __syncthreads(); }
-        if (blockSize >= 4096) { if (tid < 2048) { sdata[tid] = max(sdata[tid], sdata[tid + 2048]); } __syncthreads(); }
-        if (blockSize >= 2048) { if (tid < 1024) { sdata[tid] = max(sdata[tid], sdata[tid + 1024]); } __syncthreads(); }
-        if (blockSize >= 1024) { if (tid < 512) { sdata[tid] = max(sdata[tid], sdata[tid + 512]); } __syncthreads(); }
-        if (blockSize >= 512) { if (tid < 256) { sdata[tid] = max(sdata[tid], sdata[tid + 256]); } __syncthreads(); }
-        if (blockSize >= 256) { if (tid < 128) { sdata[tid] = max(sdata[tid], sdata[tid + 128]); } __syncthreads(); }
-        if (blockSize >= 128) { if (tid < 64) { sdata[tid] = max(sdata[tid], sdata[tid + 64]); } __syncthreads(); }
+        if (blockSize >= 8192) { if (tid < 4096) { sdata[tid] = min(sdata[tid], sdata[tid + 4096]); } __syncthreads(); }
+        if (blockSize >= 4096) { if (tid < 2048) { sdata[tid] = min(sdata[tid], sdata[tid + 2048]); } __syncthreads(); }
+        if (blockSize >= 2048) { if (tid < 1024) { sdata[tid] = min(sdata[tid], sdata[tid + 1024]); } __syncthreads(); }
+        if (blockSize >= 1024) { if (tid < 512) { sdata[tid] = min(sdata[tid], sdata[tid + 512]); } __syncthreads(); }
+        if (blockSize >= 512) { if (tid < 256) { sdata[tid] = min(sdata[tid], sdata[tid + 256]); } __syncthreads(); }
+        if (blockSize >= 256) { if (tid < 128) { sdata[tid] = min(sdata[tid], sdata[tid + 128]); } __syncthreads(); }
+        if (blockSize >= 128) { if (tid < 64) { sdata[tid] = min(sdata[tid], sdata[tid + 64]); } __syncthreads(); }
 
-        if (tid < 32) warp_reduce_velocity<blockSize>(sdata, tid);
+        if (tid < 32) warp_reduce_delta_t<blockSize>(sdata, tid);
         if (tid == 0) g_odata[blockIdx.x] = sdata[0];
     }
 }
--- tests/reduce_test.cu
+++ tests/reduce_test.cu
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cmath>
 #include <array>
+#include <limits>
 #include "float_types.h"
 #include "NDG_t.cuh"
 #include "Mesh_t.cuh"
@@ -15,6 +16,7 @@ TEST_CASE("Reduction", "Checks the reduction returns the right result."){
     const size_t N_interpolation_points = N_max * 8;
     const std::array<deviceFloat, 2> x {-1.0, 1.0};
     const double error = 1e-4;
+    const deviceFloat CFL = 0.5f;
     
     NDG_t<LegendrePolynomial_t> NDG(N_max, N_interpolation_points);
     Mesh_t mesh(N_elements, N_test, x[0], x[1]);
@@ -26,16 +28,19 @@ TEST_CASE("Reduction", "Checks the reduction returns the right result."){
     deviceFloat* host_g_odata = new deviceFloat[elements_numBlocks];
     cudaMalloc(&g_odata, elements_numBlocks * sizeof(deviceFloat));
 
-    SEM::reduce_velocity<elements_blockSize><<<elements_numBlocks, elements_blockSize>>>(mesh.N_elements_, mesh.elements_, g_odata);
+    SEM::reduce_delta_t<elements_blockSize><<<elements_numBlocks, elements_blockSize>>>(CFL, mesh.N_elements_, mesh.elements_, g_odata);
     cudaMemcpy(host_g_odata, g_odata, elements_numBlocks * sizeof(deviceFloat), cudaMemcpyDeviceToHost);
 
-    deviceFloat phi_max = 0.0;
+    deviceFloat delta_t_min = std::numeric_limits<deviceFloat>::infinity();
     for (int i = 0; i < elements_numBlocks; ++i) {
-        phi_max = max(phi_max, host_g_odata[i]);
+        delta_t_min = min(delta_t_min, host_g_odata[i]);
     }
 
-    constexpr deviceFloat target = 1.0;
-    REQUIRE(std::abs(phi_max - target) < error);
+    const deviceFloat u_max = 1.0;
+    const deviceFloat delta_x = (x[1] - x[0])/N_elements;
+    const deviceFloat delta_t = CFL * delta_x/(u_max * N_test * N_test);
+
+    REQUIRE(std::abs(delta_t_min - delta_t) < error);
 
     delete[] host_g_odata;
     cudaFree(g_odata);
