403c684e1a18ebeb1f4027a85b90c995b4b94d0e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-03-04 13:15:27 -0500

    turne out sections are NOT woing what I thought

--- tests/initial_conditions_test.cu
+++ tests/initial_conditions_test.cu
@@ -10,9 +10,7 @@
 #include "Element_t.cuh"
 #include "float_types.h"
 
-constexpr double pi = 3.14159265358979323846;
-
-TEST_CASE("Initial conditions solution value", "Checks the node and boundary values are correct after initial conditions.") {
+TEST_CASE("Initial conditions solution value", "Checks the node values are correct after initial conditions.") {   
     MPI_Init(0, nullptr);
 
     const size_t N_elements = 16;
@@ -30,8 +28,6 @@ TEST_CASE("Initial conditions solution value", "Checks the node and boundary val
     SEM::NDG_t<SEM::LegendrePolynomial_t> NDG(N_max, N_interpolation_points, stream);
     SEM::Mesh_t mesh(N_elements, N_test, x_span[0], x_span[1], stream);
     mesh.set_initial_conditions(NDG.nodes_);
-    cudaDeviceSynchronize();
-    SEM::interpolate_to_boundaries<<<mesh.elements_numBlocks_, mesh.elements_blockSize_, 0, stream>>>(mesh.N_elements_, mesh.elements_, NDG.lagrange_interpolant_left_, NDG.lagrange_interpolant_right_, NDG.lagrange_interpolant_derivative_left_, NDG.lagrange_interpolant_derivative_right_);
     cudaDeviceSynchronize();
     
     deviceFloat* x;
@@ -55,7 +51,7 @@ TEST_CASE("Initial conditions solution value", "Checks the node and boundary val
     deviceFloat* host_sigma = new deviceFloat[mesh.N_elements_];
     bool* host_refine = new bool[mesh.N_elements_];
     bool* host_coarsen = new bool[mesh.N_elements_];
-    deviceFloat* host_error = new deviceFloat[mesh.N_elements_ ];
+    deviceFloat* host_error = new deviceFloat[mesh.N_elements_];
     cudaMalloc(&x, mesh.N_elements_ * N_interpolation_points * sizeof(deviceFloat));
     cudaMalloc(&phi, mesh.N_elements_ * N_interpolation_points * sizeof(deviceFloat));
     cudaMalloc(&phi_prime, mesh.N_elements_ * N_interpolation_points * sizeof(deviceFloat));
@@ -82,48 +78,11 @@ TEST_CASE("Initial conditions solution value", "Checks the node and boundary val
     cudaMemcpy(host_coarsen, coarsen, mesh.N_elements_ * sizeof(bool), cudaMemcpyDeviceToHost);
     cudaMemcpy(host_error, error, mesh.N_elements_ * sizeof(deviceFloat), cudaMemcpyDeviceToHost);
 
-    SECTION("Interpolated phi") {
-        for (int i = 0; i < N_elements * N_interpolation_points; ++i) {
-            const double expected = SEM::g(host_x[i]);
-            REQUIRE(std::abs(expected - host_phi[i]) < max_error);
-        }
+    for (int i = 0; i < N_elements * N_interpolation_points; ++i) {
+        const double expected = SEM::g(host_x[i]);
+        REQUIRE(std::abs(expected - host_phi[i]) < max_error);
     }
 
-    SEM::Face_t* host_faces = new SEM::Face_t[mesh.N_faces_];
-    SEM::Element_t* host_elements = new SEM::Element_t[mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_];
-    cudaMemcpy(host_faces, mesh.faces_, mesh.N_faces_ * sizeof(SEM::Face_t), cudaMemcpyDeviceToHost);
-    cudaMemcpy(host_elements, mesh.elements_, (mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_) * sizeof(SEM::Element_t), cudaMemcpyDeviceToHost);
-    // Invalidate GPU pointers, or else they will be deleted on the CPU, where they point to random stuff
-    for (size_t i = 0; i < mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_; ++i) {
-        host_elements[i].phi_ = nullptr;
-        host_elements[i].q_ = nullptr;
-        host_elements[i].ux_ = nullptr;
-        host_elements[i].phi_prime_ = nullptr;
-        host_elements[i].intermediate_ = nullptr;
-    }
-
-    SECTION("Extrapolated phi") {
-        for (int i = 0; i < N_elements; ++i) {
-            const double phi_L_expected = SEM::g(host_elements[i].x_[0]);
-            const double phi_R_expected = SEM::g(host_elements[i].x_[1]);
-
-            REQUIRE(std::abs(phi_L_expected - host_elements[i].phi_L_) < max_error);
-            REQUIRE(std::abs(phi_R_expected - host_elements[i].phi_R_) < max_error);
-        }
-    }
-
-    SECTION("Extrapolated phi prime") {
-        for (int i = 0; i < N_elements; ++i) {
-            const double phi_prime_L_expected = SEM::g_prime(host_elements[i].x_[0]);
-            const double phi_prime_R_expected = SEM::g_prime(host_elements[i].x_[1]);
-
-            REQUIRE(std::abs(phi_prime_L_expected - host_elements[i].phi_prime_L_) < max_error);
-            REQUIRE(std::abs(phi_prime_R_expected - host_elements[i].phi_prime_R_) < max_error);
-        }
-    }
-
-    delete[] host_faces;
-    delete[] host_elements;
     delete[] host_x;
     delete[] host_phi;
     delete[] host_phi_prime;
@@ -147,4 +106,60 @@ TEST_CASE("Initial conditions solution value", "Checks the node and boundary val
     cudaFree(coarsen);
     cudaFree(error);
     MPI_Finalize();
+}
+
+TEST_CASE("Initial conditions boundary values", "Checks the extrapolated boundary values are correct after initial conditions.") {   
+    MPI_Init(0, nullptr);
+
+    const size_t N_elements = 16;
+    const int N_max = 16;
+    const int N_test = 16;
+    const size_t N_interpolation_points = N_max * 8;
+    const std::array<deviceFloat, 2> x_span {-1.0, 1.0};
+    const double max_error = 1e-6;
+
+    REQUIRE(N_test <= N_max);
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream); 
+    
+    SEM::NDG_t<SEM::LegendrePolynomial_t> NDG(N_max, N_interpolation_points, stream);
+    SEM::Mesh_t mesh(N_elements, N_test, x_span[0], x_span[1], stream);
+    mesh.set_initial_conditions(NDG.nodes_);
+    cudaDeviceSynchronize();
+    SEM::interpolate_to_boundaries<<<mesh.elements_numBlocks_, mesh.elements_blockSize_, 0, stream>>>(mesh.N_elements_, mesh.elements_, NDG.lagrange_interpolant_left_, NDG.lagrange_interpolant_right_, NDG.lagrange_interpolant_derivative_left_, NDG.lagrange_interpolant_derivative_right_);
+    cudaDeviceSynchronize();
+
+    SEM::Face_t* host_faces = new SEM::Face_t[mesh.N_faces_];
+    SEM::Element_t* host_elements = new SEM::Element_t[mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_];
+    cudaMemcpy(host_faces, mesh.faces_, mesh.N_faces_ * sizeof(SEM::Face_t), cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_elements, mesh.elements_, (mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_) * sizeof(SEM::Element_t), cudaMemcpyDeviceToHost);
+    // Invalidate GPU pointers, or else they will be deleted on the CPU, where they point to random stuff
+    for (size_t i = 0; i < mesh.N_elements_ + mesh.N_local_boundaries_ + mesh.N_MPI_boundaries_; ++i) {
+        host_elements[i].phi_ = nullptr;
+        host_elements[i].q_ = nullptr;
+        host_elements[i].ux_ = nullptr;
+        host_elements[i].phi_prime_ = nullptr;
+        host_elements[i].intermediate_ = nullptr;
+    }
+    
+    for (int i = 0; i < N_elements; ++i) {
+        const double phi_L_expected = SEM::g(host_elements[i].x_[0]);
+        const double phi_R_expected = SEM::g(host_elements[i].x_[1]);
+
+        REQUIRE(std::abs(phi_L_expected - host_elements[i].phi_L_) < max_error);
+        REQUIRE(std::abs(phi_R_expected - host_elements[i].phi_R_) < max_error);
+    }
+
+    for (int i = 0; i < N_elements; ++i) {
+        const double phi_prime_L_expected = SEM::g_prime(host_elements[i].x_[0]);
+        const double phi_prime_R_expected = SEM::g_prime(host_elements[i].x_[1]);
+        std::cout << "Element " << i << " phi_prime: " << host_elements[i].phi_prime_L_ << "    " << host_elements[i].phi_prime_R_ << std::endl;
+
+        REQUIRE(std::abs(phi_prime_L_expected - host_elements[i].phi_prime_L_) < max_error);
+        REQUIRE(std::abs(phi_prime_R_expected - host_elements[i].phi_prime_R_) < max_error);
+    }
+
+    delete[] host_faces;
+    delete[] host_elements;
 }
