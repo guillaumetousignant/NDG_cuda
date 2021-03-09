c5b02fceefcb747d09667c8bb098ee061d5a84a2
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-03-09 14:11:49 -0500

    up to 1s

--- src/main.cu
+++ src/main.cu
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     const std::array<deviceFloat, 2> x {-1.0, 1.0};
     const deviceFloat CFL = 0.1f;
     const deviceFloat viscosity = 1.0f;
-    std::vector<deviceFloat> output_times{0.1f, 0.2f, 0.3f, 0.4f};
+    std::vector<deviceFloat> output_times{0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f};
 
     const int initial_N = 6;
     const size_t N_interpolation_points = N_max * 8;
