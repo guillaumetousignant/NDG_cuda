5e728f826824718be951734f233bf70063d86d21
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-04-14 15:15:32 -0400

    added connectivity start

--- extras/mesh_generator/main.cpp
+++ extras/mesh_generator/main.cpp
@@ -18,15 +18,18 @@ int main(int argc, char* argv[]) {
     fs::create_directory(save_dir);
     const fs::path save_file = save_dir / filename;
 
-    const std::string input_x_res = input_parser.getCmdOption("--xres");
-    const int x_res = (input_x_res.empty()) ? 4 : std::stoi(input_x_res);
+    const std::string input_res = input_parser.getCmdOption("--resolution");
+    const int res = (input_res.empty()) ? 4 : std::stoi(input_res);
 
-    const std::string input_y_res = input_parser.getCmdOption("--yres");
-    const int y_res = (input_y_res.empty()) ? 4 : std::stoi(input_y_res);
+    const int x_res = res;
+    const int y_res = res;
+
+    const int n_elements = x_res * y_res;
+    const int n_nodes = (x_res + 1) * (y_res + 1);
 
     /* create gridpoints for simple example: */
-    std::vector<double> x((x_res + 1) * (y_res + 1));
-    std::vector<double> y((x_res + 1) * (y_res + 1));
+    std::vector<double> x(n_nodes);
+    std::vector<double> y(n_nodes);
 
     for (int i = 0; i < x_res + 1; ++i)
     {
@@ -44,21 +47,21 @@ int main(int argc, char* argv[]) {
     if (cg_open(save_file.string().c_str(), CG_MODE_WRITE, &index_file)) cg_error_exit();
 
     /* create base (user can give any name) */
-    std::string base_name("Base");
+    const std::string base_name("Base");
     const int icelldim = 2;
     const int iphysdim = 2;
     int index_base;
     cg_base_write(index_file, base_name.c_str(), icelldim, iphysdim, &index_base);
 
     /* define zone name (user can give any name) */
-    std::string zone_name("Zone  1");
+    const std::string zone_name("Zone  1");
 
     /* vertex size */
     cgsize_t isize[3];
-    isize[0] = (x_res + 1) * (y_res + 1);
+    isize[0] = n_nodes;
 
     /* cell size */
-    isize[1] = x_res * y_res;
+    isize[1] = n_elements;
 
     /* boundary vertex size (always zero for structured grids) */
     isize[2] = 0;
@@ -71,6 +74,22 @@ int main(int argc, char* argv[]) {
     int index_coord;
     cg_coord_write(index_file, index_base, index_zone, RealDouble, "CoordinateX", x.data(), &index_coord);
     cg_coord_write(index_file, index_base, index_zone, RealDouble, "CoordinateY", y.data(), &index_coord);
+
+    /* set element connectivity */
+    constexpr int n_sides = 4;
+    std::vector<int> elements(n_sides * n_elements);
+
+    for (int i = 0; i < n_elements; ++i) {
+
+    }
+
+    /* write HEX_8 element connectivity (user can give any name) */
+    const std::string elements_name("Elements");
+    int index_section;
+    const int nelem_start = 0;
+    const int nelem_end = n_elements - 1;
+    const int n_boundary_elem = 0; // No boundaries yet
+    cg_section_write(index_file, index_base, index_zone, elements_name.c_str(), QUAD_4, nelem_start, nelem_end, n_boundary_elem, elements, &index_section);
 
     /* close CGNS file */
     cg_close(index_file);
