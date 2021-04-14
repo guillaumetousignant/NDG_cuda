7d3b456d915d2f9e0e05ae1f0811fbd5680a6672
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-04-14 15:36:19 -0400

    connectivity

--- extras/mesh_generator/main.cpp
+++ extras/mesh_generator/main.cpp
@@ -1,12 +1,21 @@
 #include "helpers/InputParser_t.h"
+#include "functions/Hilbert.h"
 #include "cgnslib.h"
 #include <string>
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <array>
 
 namespace fs = std::filesystem;
 
-int main(int argc, char* argv[]) {
+/* Function to check if x is power of 2*/
+auto isPowerOfTwo (int x) -> bool {
+    /* First x in the below expression is for the case when x is 0 */
+    return x && (!(x&(x-1)));
+}
+ 
+auto main(int argc, char* argv[]) -> int {
     const SEM::Helpers::InputParser_t input_parser(argc, argv);
 
     const std::string input_filename = input_parser.getCmdOption("--filename");
@@ -20,6 +29,11 @@ int main(int argc, char* argv[]) {
 
     const std::string input_res = input_parser.getCmdOption("--resolution");
     const int res = (input_res.empty()) ? 4 : std::stoi(input_res);
+
+    if (!isPowerOfTwo(res)) {
+        std::cerr << "Error, grid resolution should be a power of two for the Hillbert curve to work. Input resolution: " << res << "'. Exiting." << std::endl;
+        exit(3);
+    }
 
     const int x_res = res;
     const int y_res = res;
@@ -80,7 +94,11 @@ int main(int argc, char* argv[]) {
     std::vector<int> elements(n_sides * n_elements);
 
     for (int i = 0; i < n_elements; ++i) {
-
+        const std::array<int, 2> xy = SEM::Hilbert::d2xy(res, i);
+        elements[i * n_sides]     = (x_res + 1) * (xy[0] + 1) + xy[1];
+        elements[i * n_sides + 1] = (x_res + 1) * (xy[0] + 1) + xy[1] + 1;
+        elements[i * n_sides + 2] = (x_res + 1) * xy[0] + xy[1] + 1;
+        elements[i * n_sides + 3] = (x_res + 1) * xy[0] + xy[1];
     }
 
     /* write HEX_8 element connectivity (user can give any name) */
@@ -89,7 +107,7 @@ int main(int argc, char* argv[]) {
     const int nelem_start = 0;
     const int nelem_end = n_elements - 1;
     const int n_boundary_elem = 0; // No boundaries yet
-    cg_section_write(index_file, index_base, index_zone, elements_name.c_str(), QUAD_4, nelem_start, nelem_end, n_boundary_elem, elements, &index_section);
+    cg_section_write(index_file, index_base, index_zone, elements_name.c_str(), QUAD_4, nelem_start, nelem_end, n_boundary_elem, elements.data(), &index_section);
 
     /* close CGNS file */
     cg_close(index_file);
