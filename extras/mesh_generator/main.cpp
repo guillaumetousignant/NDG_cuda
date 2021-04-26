f5b02a17d4db51ee54423669c44ca66f60e1ac81
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-04-26 11:11:26 -0400

    Added help input argument, and changed erroneous 'symmetry' wording to periodic

--- extras/mesh_generator/main.cpp
+++ extras/mesh_generator/main.cpp
@@ -59,72 +59,65 @@ auto get_boundary_conditions(const SEM::Helpers::InputParser_t& input_parser) ->
         }
     }
     else {
-        std::string input_bottom_boundary = input_parser.getCmdOption("--bottom_boundary");
-        if (input_bottom_boundary.empty()) {
-            bottom_boundary_type = BCType_t::BCWall;
+        bottom_boundary_type = BCType_t::BCWall;
+        right_boundary_type  = BCType_t::BCWall;
+        top_boundary_type    = BCType_t::BCWall;
+        left_boundary_type   = BCType_t::BCWall;
+    }
+    
+    std::string input_bottom_boundary = input_parser.getCmdOption("--bottom_boundary");
+    if (!input_bottom_boundary.empty()) {
+        SEM::to_lower(input_bottom_boundary);
+
+        const auto it = values.find(input_bottom_boundary);
+        if (it == values.end()) {
+            std::cerr << "Error, unknown bottom boundary type '" << input_bottom_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
+            exit(5);
         }
         else {
-            SEM::to_lower(input_bottom_boundary);
-
-            const auto it = values.find(input_bottom_boundary);
-            if (it == values.end()) {
-                std::cerr << "Error, unknown bottom boundary type '" << input_bottom_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
-                exit(5);
-            }
-            else {
-                bottom_boundary_type = it->second;
-            }
+            bottom_boundary_type = it->second;
         }
+    }
 
-        std::string input_right_boundary = input_parser.getCmdOption("--right_boundary");
-        if (input_right_boundary.empty()) {
-            right_boundary_type = BCType_t::BCWall;
+    std::string input_right_boundary = input_parser.getCmdOption("--right_boundary");
+    if (!input_right_boundary.empty()) {
+        SEM::to_lower(input_right_boundary);
+
+        const auto it = values.find(input_right_boundary);
+        if (it == values.end()) {
+            std::cerr << "Error, unknown right boundary type '" << input_right_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
+            exit(6);
         }
         else {
-            SEM::to_lower(input_right_boundary);
-
-            const auto it = values.find(input_right_boundary);
-            if (it == values.end()) {
-                std::cerr << "Error, unknown right boundary type '" << input_right_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
-                exit(6);
-            }
-            else {
-                right_boundary_type = it->second;
-            }
+            right_boundary_type = it->second;
         }
+    }
 
-        std::string input_top_boundary = input_parser.getCmdOption("--top_boundary");
-        if (input_top_boundary.empty()) {
-            top_boundary_type = BCType_t::BCWall;
+    std::string input_top_boundary = input_parser.getCmdOption("--top_boundary");
+    if (!input_top_boundary.empty()) {
+        SEM::to_lower(input_top_boundary);
+
+        const auto it = values.find(input_top_boundary);
+        if (it == values.end()) {
+            std::cerr << "Error, unknown top boundary type '" << input_top_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
+            exit(7);
         }
         else {
-            SEM::to_lower(input_top_boundary);
-
-            const auto it = values.find(input_top_boundary);
-            if (it == values.end()) {
-                std::cerr << "Error, unknown top boundary type '" << input_top_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
-                exit(7);
-            }
-            else {
-                top_boundary_type = it->second;
-            }
+            top_boundary_type = it->second;
         }
+    }
 
-        std::string input_left_boundary = input_parser.getCmdOption("--left_boundary");
-        if (input_left_boundary.empty()) {
-            left_boundary_type = BCType_t::BCWall;
+    std::string input_left_boundary = input_parser.getCmdOption("--left_boundary");
+    if (!input_left_boundary.empty()) {
+        SEM::to_lower(input_left_boundary);
+
+        const auto it = values.find(input_left_boundary);
+        if (it == values.end()) {
+            std::cerr << "Error, unknown left boundary type '" << input_left_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
+            exit(8);
         }
         else {
-            SEM::to_lower(input_left_boundary);
-
-            const auto it = values.find(input_left_boundary);
-            if (it == values.end()) {
-                std::cerr << "Error, unknown left boundary type '" << input_left_boundary << "'. Implemented boundary types are: 'wall', 'symmetry'. Exiting." << std::endl;
-                exit(8);
-            }
-            else {
-                left_boundary_type = it->second;
-            }
+            left_boundary_type = it->second;
         }
     }
 
@@ -133,6 +126,22 @@ auto get_boundary_conditions(const SEM::Helpers::InputParser_t& input_parser) ->
  
 auto main(int argc, char* argv[]) -> int {
     const SEM::Helpers::InputParser_t input_parser(argc, argv);
+    if (input_parser.cmdOptionExists("--help") || input_parser.cmdOptionExists("-h")) {
+        std::cout << "Square unstructured mesh generator" << std::endl;
+        std::cout << '\t' << "Available options:" << std::endl;
+        std::cout << '\t' << '\t' <<  "--path"            <<  '\t' <<  "Full path to the output mesh file. Overrides filename and directory if set." << std::endl;
+        std::cout << '\t' << '\t' <<  "--filename"        <<  '\t' <<  "File name to the output mesh file. Defaults to [mesh.cgns]" << std::endl;
+        std::cout << '\t' << '\t' <<  "--directory"       <<  '\t' <<  "Directory in which to save the output mesh file. Defaults to [./meshes/]" << std::endl;
+        std::cout << '\t' << '\t' <<  "--resolution"      <<  '\t' <<  "Number of elements in the x and y directions. Must be a power of two. Defaults to [4]" << std::endl;
+        std::cout << '\t' << '\t' <<  "--x_periodic"      <<  '\t' <<  "Sets the mesh to be periodic in the x direction. Overrides left and right boundaries." << std::endl;
+        std::cout << '\t' << '\t' <<  "--y_periodic"      <<  '\t' <<  "Sets the mesh to be periodic in the y direction. Overrides top and bottom boundaries." << std::endl;
+        std::cout << '\t' << '\t' <<  "--boundaries"      <<  '\t' <<  "Sets all four boundary conditions. Acceptable values are \"wall\", \"symmetry\" and \"null\". Defaults to [wall]" << std::endl;
+        std::cout << '\t' << '\t' <<  "--bottom_boundary" <<  '\t' <<  "Sets the bottom boundary condition. Acceptable values are \"wall\", \"symmetry\" and \"null\". Overrides boundaries." << std::endl;
+        std::cout << '\t' << '\t' <<  "--right_boundary"  <<  '\t' <<  "Sets the right boundary condition. Acceptable values are \"wall\", \"symmetry\" and \"null\". Overrides boundaries." << std::endl;
+        std::cout << '\t' << '\t' <<  "--top_boundary"    <<  '\t' <<  "Sets the top boundary condition. Acceptable values are \"wall\", \"symmetry\" and \"null\". Overrides boundaries." << std::endl;
+        std::cout << '\t' << '\t' <<  "--left_boundary"   <<  '\t' <<  "Sets the left boundary condition. Acceptable values are \"wall\", \"symmetry\" and \"null\". Overrides boundaries." << std::endl;
+        exit(0);
+    }
 
     const fs::path save_file = get_save_file(input_parser);
 
@@ -159,8 +168,8 @@ auto main(int argc, char* argv[]) -> int {
     const auto [bottom_boundary_type, right_boundary_type, top_boundary_type, left_boundary_type] = get_boundary_conditions(input_parser);
 
     // Symmetry input
-    const bool x_symmetry = input_parser.cmdOptionExists("--x_symmetry");
-    const bool y_symmetry = input_parser.cmdOptionExists("--y_symmetry");
+    const bool x_periodic = input_parser.cmdOptionExists("--x_periodic");
+    const bool y_periodic = input_parser.cmdOptionExists("--y_periodic");
 
     /* create gridpoints for simple example: */
     std::vector<double> x(n_nodes);
@@ -189,7 +198,7 @@ auto main(int argc, char* argv[]) -> int {
     cg_base_write(index_file, base_name.c_str(), icelldim, iphysdim, &index_base);
 
     /* define zone name (user can give any name) */
-    const std::string zone_name("Zone  1");
+    const std::string zone_name("Zone 1");
 
     /* vertex size, cell size, boundary vertex size (always zero for structured grids) */
     std::array<cgsize_t, 3> isize {n_nodes,
@@ -282,7 +291,7 @@ auto main(int argc, char* argv[]) -> int {
 
     /* write boundary conditions */
     /* the above are all face-center locations for the BCs - must indicate this, otherwise Vertices will be assumed! */
-    if (!y_symmetry) {
+    if (!y_periodic) {
         int bottom_index_boundary = 0;
         std::vector<int> bottom_boundary(x_res);
 
@@ -290,7 +299,7 @@ auto main(int argc, char* argv[]) -> int {
             bottom_boundary[i] = bottom_start_index + i;
         }
 
-        const int err = cg_boco_write(index_file, index_base, index_zone, "BottomBoundary", bottom_boundary_type, PointSetType_t::PointList, x_res, bottom_boundary.data(), &bottom_index_boundary);
+        cg_boco_write(index_file, index_base, index_zone, "BottomBoundary", bottom_boundary_type, PointSetType_t::PointList, x_res, bottom_boundary.data(), &bottom_index_boundary);
         cg_boco_gridlocation_write(index_file, index_base, index_zone, bottom_index_boundary, GridLocation_t::EdgeCenter);
 
         int top_index_boundary = 0;
@@ -304,7 +313,7 @@ auto main(int argc, char* argv[]) -> int {
         cg_boco_gridlocation_write(index_file, index_base, index_zone, top_index_boundary, GridLocation_t::EdgeCenter);
     }
 
-    if (!x_symmetry) {
+    if (!x_periodic) {
         int right_index_boundary = 0;
         std::vector<int> right_boundary(y_res);
 
@@ -327,9 +336,9 @@ auto main(int argc, char* argv[]) -> int {
     }
 
     /* write integer connectivity info (user can give any name) */
-    if (y_symmetry) {
-        int y_symmetry_bottom_index = 0;
-        int y_symmetry_top_index = 0;
+    if (y_periodic) {
+        int y_periodic_bottom_index = 0;
+        int y_periodic_top_index = 0;
         std::vector<int> elements_bottom(x_res);
         std::vector<int> elements_top(x_res);
 
@@ -338,13 +347,13 @@ auto main(int argc, char* argv[]) -> int {
             elements_top[i] = top_end_index - i;
         }
 
-        cg_conn_write(index_file, index_base, index_zone, "YSymmetryBottom", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, x_res, elements_bottom.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, x_res, elements_top.data(), &y_symmetry_bottom_index);
-        cg_conn_write(index_file, index_base, index_zone, "YSymmetryTop", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, x_res, elements_top.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, x_res, elements_bottom.data(), &y_symmetry_top_index);
+        cg_conn_write(index_file, index_base, index_zone, "YPeriodicBottom", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, x_res, elements_bottom.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, x_res, elements_top.data(), &y_periodic_bottom_index);
+        cg_conn_write(index_file, index_base, index_zone, "YPeriodicTop", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, x_res, elements_top.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, x_res, elements_bottom.data(), &y_periodic_top_index);
     }
 
-    if (x_symmetry) {
-        int x_symmetry_right_index = 0;
-        int x_symmetry_left_index = 0;
+    if (x_periodic) {
+        int x_periodic_right_index = 0;
+        int x_periodic_left_index = 0;
         std::vector<int> elements_right(y_res);
         std::vector<int> elements_left(y_res);
 
@@ -353,8 +362,8 @@ auto main(int argc, char* argv[]) -> int {
             elements_left[j] = left_end_index - j;
         }
 
-        cg_conn_write(index_file, index_base, index_zone, "XSymmetryRight", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, y_res, elements_right.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, y_res, elements_left.data(), &x_symmetry_right_index);
-        cg_conn_write(index_file, index_base, index_zone, "XSymmetryLeft", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, y_res, elements_left.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, y_res, elements_right.data(), &x_symmetry_left_index);
+        cg_conn_write(index_file, index_base, index_zone, "XPeriodicRight", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, y_res, elements_right.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, y_res, elements_left.data(), &x_periodic_right_index);
+        cg_conn_write(index_file, index_base, index_zone, "XPeriodicLeft", GridLocation_t::FaceCenter, GridConnectivityType_t::Abutting1to1, PointSetType_t::PointList, y_res, elements_left.data(), zone_name.c_str(), ZoneType_t::Unstructured, PointSetType_t::PointListDonor, DataType_t::Integer, y_res, elements_right.data(), &x_periodic_left_index);
     }
 
     /* close CGNS file */
