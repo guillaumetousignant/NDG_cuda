1638ea9974541ca4345599c440dfd87d1d56391b
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-06-03 11:41:01 -0400

    reading mesh

--- extras/mesh_partitioner/main.cpp
+++ extras/mesh_partitioner/main.cpp
@@ -3,8 +3,12 @@
 #include <string>
 #include <iostream>
 #include <filesystem>
+#include <array>
+#include <vector>
 
 namespace fs = std::filesystem;
+
+constexpr int CGIO_MAX_NAME_LENGTH = 33; // Includes the null terminator
 
 auto main(int argc, char* argv[]) -> int {
     fs::path in_file;
@@ -43,6 +47,214 @@ auto main(int argc, char* argv[]) -> int {
         fs::create_directory(save_dir);
         out_file = save_dir / save_filename;
     }
+
+    int index_file = 0;
+    const int open_error = cg_open(in_file.string().c_str(), CG_MODE_READ, &index_file);
+    if (open_error != CG_OK) {
+        std::cerr << "Error: file '" << in_file << "' could not be opened with error '" << cg_get_error() << "'. Exiting." << std::endl;
+        exit(16);
+    }
     
+    // Getting base information
+    int n_bases = 0;
+    cg_nbases(index_file, &n_bases);
+    if (n_bases != 1) {
+        std::cerr << "Error: CGNS mesh has " << n_bases << " base(s), but for now only a single base is supported. Exiting." << std::endl;
+        exit(17);
+    }
+    constexpr int index_base = 1;
+
+    std::array<char, CGIO_MAX_NAME_LENGTH> base_name; // Oh yeah cause it's the 80s still
+    int dim = 0;
+    int physDim = 0;
+    cg_base_read(index_file, index_base, base_name.data(), &dim, &physDim);
+    if (dim != 2) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << " has " << dim << " dimensions, but the program only supports 2 dimensions. Exiting." << std::endl;
+        exit(18);
+    }
+    if (physDim != 2) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << " has " << physDim << " physical dimensions, but the program only supports 2 physical dimensions. Exiting." << std::endl;
+        exit(19);
+    }
+
+    // Getting zone information
+    int n_zones = 0;
+    cg_nzones(index_file, index_base, &n_zones);
+    if (n_bases != 1) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << " has " << n_zones << " zone(s), but for now only a single zone is supported. Exiting." << std::endl;
+        exit(20);
+    }
+    constexpr int index_zone = 1;
+
+    ZoneType_t zone_type = ZoneType_t::ZoneTypeNull;
+    cg_zone_type(index_file, index_base, index_zone, &zone_type);
+    if (zone_type != ZoneType_t::Unstructured) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << " is not an unstructured zone. For now only unstructured zones are supported. Exiting." << std::endl;
+        exit(21);
+    }
+
+    std::array<char, CGIO_MAX_NAME_LENGTH> zone_name; // Oh yeah cause it's the 80s still
+    std::array<int, 3> isize{0, 0, 0};
+    cg_zone_read(index_file, index_base, index_zone, zone_name.data(), isize.data());
+    if (isize[2] != 0) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << " has " << isize[2] << " boundary vertices, but to be honest I'm not sure how to deal with them. Exiting." << std::endl;
+        exit(22);
+    }
+    const int n_nodes = isize[0];
+    const int n_elements = isize[1];
+
+    // Getting nodes
+    int n_coords = 0;
+    cg_ncoords(index_file, index_base, index_zone, &n_coords);
+    if (n_coords != 2) {
+        std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << " has " << n_coords << " sets of coordinates, but for now only two are supported. Exiting." << std::endl;
+        exit(23);
+    }
+
+    std::array<std::array<char, CGIO_MAX_NAME_LENGTH>, 2> coord_names; // Oh yeah cause it's the 80s still
+    std::array<DataType_t, 2> coord_data_types {DataType_t::DataTypeNull, DataType_t::DataTypeNull};
+    for (int index_coord = 1; index_coord <= n_coords; ++index_coord) {
+        cg_coord_info(index_file, index_base, index_zone, index_coord, &coord_data_types[index_coord - 1], coord_names[index_coord - 1].data());
+    }
+
+    std::array<std::vector<double>, 2> xy{std::vector<double>(n_nodes), std::vector<double>(n_nodes)};
+
+    for (int index_coord = 1; index_coord <= n_coords; ++index_coord) {
+        const int index_coord_start = 1;
+        cg_coord_read(index_file, index_base, index_zone, coord_names[index_coord - 1].data(), DataType_t::RealDouble, &index_coord_start, &n_nodes, xy[index_coord - 1].data());
+    }
+
+    // Getting connectivity
+    int n_sections = 0;
+    cg_nsections(index_file, index_base, index_zone, &n_sections);
+
+    std::vector<int> section_data_size(n_sections);
+    std::vector<std::array<char, CGIO_MAX_NAME_LENGTH>> section_names(n_sections); // Oh yeah cause it's the 80s still
+    std::vector<ElementType_t> section_type(n_sections);
+    std::vector<std::array<int, 2>> section_ranges(n_sections);
+    std::vector<int> section_n_boundaries(n_sections);
+    std::vector<int> section_parent_flags(n_sections);
+    for (int index_section = 1; index_section <= n_sections; ++index_section) {
+        cg_ElementDataSize(index_file, index_base, index_zone, index_section, &section_data_size[index_section - 1]);
+        cg_section_read(index_file, index_base, index_zone, index_section, section_names[index_section - 1].data(), &section_type[index_section - 1], &section_ranges[index_section - 1][0], &section_ranges[index_section - 1][1], &section_n_boundaries[index_section - 1], &section_parent_flags[index_section - 1]);
+        if (section_n_boundaries[index_section - 1] != 0) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", section " << index_section << " has " << section_n_boundaries[index_section - 1] << " boundary elements, but to be honest I'm not sure how to deal with them. Exiting." << std::endl;
+            exit(24);
+        }
+    }
+
+    std::vector<std::vector<int>> connectivity(n_sections);
+    std::vector<std::vector<int>> parent_data(n_sections);
+    for (int index_section = 1; index_section <= n_sections; ++index_section) {
+        connectivity[index_section - 1] = std::vector<int>(section_data_size[index_section - 1]);
+        parent_data[index_section - 1] = std::vector<int>(section_ranges[index_section - 1][1] - section_ranges[index_section - 1][0]);
+
+        cg_elements_read(index_file, index_base, index_zone, index_section, connectivity[index_section - 1].data(), parent_data[index_section - 1].data());
+    }
+
+    // Interfaces
+    int n_connectivity = 0;
+    cg_nconns(index_file, index_base, index_zone, &n_connectivity);
+
+    std::vector<std::array<char, CGIO_MAX_NAME_LENGTH>> connectivity_names(n_connectivity); // Oh yeah cause it's the 80s still
+    std::vector<GridLocation_t> connectivity_grid_locations(n_connectivity);
+    std::vector<GridConnectivityType_t> connectivity_types(n_connectivity);
+    std::vector<PointSetType_t> connectivity_point_set_types(n_connectivity);
+    std::vector<int> connectivity_sizes(n_connectivity);
+    std::vector<std::array<char, CGIO_MAX_NAME_LENGTH>> connectivity_donor_names(n_connectivity); // Oh yeah cause it's the 80s still
+    std::vector<ZoneType_t> connectivity_donor_zone_types(n_connectivity);
+    std::vector<PointSetType_t> connectivity_donor_point_set_types(n_connectivity);
+    std::vector<DataType_t> connectivity_donor_data_types(n_connectivity);
+    std::vector<int> connectivity_donor_sizes(n_connectivity);
+    for (int index_connectivity = 1; index_connectivity <= n_connectivity; ++index_connectivity) {
+        cg_conn_info(index_file, index_base, index_zone, index_connectivity, connectivity_names[index_connectivity - 1].data(),
+            &connectivity_grid_locations[index_connectivity - 1], &connectivity_types[index_connectivity - 1],
+            &connectivity_point_set_types[index_connectivity - 1], &connectivity_sizes[index_connectivity - 1], connectivity_donor_names[index_connectivity - 1].data(),
+            &connectivity_donor_zone_types[index_connectivity - 1], &connectivity_donor_point_set_types[index_connectivity - 1],
+            &connectivity_donor_data_types[index_connectivity - 1], &connectivity_donor_sizes[index_connectivity - 1]);
+
+        if (connectivity_donor_zone_types[index_connectivity - 1] != ZoneType_t::Unstructured) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a donor zone type that isn't unstructured. For now only unstructured zones are supported. Exiting." << std::endl;
+            exit(25);
+        }
+        if (connectivity_point_set_types[index_connectivity - 1] != PointSetType_t::PointList) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a point set type that isn't PointList. For now only PointList point set types are supported. Exiting." << std::endl;
+            exit(26);
+        }
+        if (connectivity_donor_point_set_types[index_connectivity - 1] != PointSetType_t::PointListDonor) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a donor point set type that isn't PointListDonor. For now only PointListDonor point set types are supported. Exiting." << std::endl;
+            exit(27);
+        }
+
+        if (connectivity_grid_locations[index_connectivity - 1] != GridLocation_t::FaceCenter) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a grid location that isn't FaceCenter. For now only FaceCenter grid locations are supported. Exiting." << std::endl;
+            exit(28);
+        }
+
+        if (connectivity_types[index_connectivity - 1] != GridConnectivityType_t::Abutting1to1) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a grid connectivity type that isn't Abutting1to1. For now only Abutting1to1 grid connectivity types are supported. Exiting." << std::endl;
+            exit(29);
+        }
+
+        if (connectivity_sizes[index_connectivity - 1] != connectivity_donor_sizes[index_connectivity - 1]) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", connectivity " << index_connectivity << " has a different number of elements in the origin and destination zones. Exiting." << std::endl;
+            exit(30);
+        }
+    }
+
+    std::vector<std::vector<int>> interface_elements(n_connectivity);
+    std::vector<std::vector<int>> interface_donor_elements(n_connectivity);
+    for (int index_connectivity = 1; index_connectivity <= n_connectivity; ++index_connectivity) {
+        interface_elements[index_connectivity - 1] = std::vector<int>(connectivity_sizes[index_connectivity - 1]);
+        interface_donor_elements[index_connectivity - 1] = std::vector<int>(connectivity_donor_sizes[index_connectivity - 1]);
+        cg_conn_read(index_file, index_base, index_zone, index_connectivity, interface_elements[index_connectivity - 1].data(),
+            DataType_t::Integer, interface_donor_elements[index_connectivity - 1].data());
+    }
+
+    
+    // Boundary conditions
+    int n_boundaries = 0;
+    cg_nbocos(index_file, index_base, index_zone, &n_boundaries);
+
+    std::vector<std::array<char, CGIO_MAX_NAME_LENGTH>> boundary_names(n_boundaries); // Oh yeah cause it's the 80s still
+    std::vector<BCType_t> boundary_types(n_boundaries);
+    std::vector<PointSetType_t> boundary_point_set_types(n_boundaries);
+    std::vector<int> boundary_sizes(n_boundaries);
+    std::vector<int> boundary_normal_indices(n_boundaries);
+    std::vector<int> boundary_normal_list_sizes(n_boundaries);
+    std::vector<DataType_t> boundary_normal_data_types(n_boundaries);
+    std::vector<int> boundary_n_datasets(n_boundaries);
+    std::vector<GridLocation_t> boundary_grid_locations(n_boundaries);
+    for (int index_boundary = 1; index_boundary <= n_boundaries; ++index_boundary) {
+        cg_boco_info(index_file, index_base, index_zone, index_boundary, boundary_names[index_boundary - 1].data(),
+            &boundary_types[index_boundary - 1], &boundary_point_set_types[index_boundary - 1], &boundary_sizes[index_boundary - 1],
+            &boundary_normal_indices[index_boundary - 1], &boundary_normal_list_sizes[index_boundary - 1],
+            &boundary_normal_data_types[index_boundary - 1], &boundary_n_datasets[index_boundary - 1]);
+
+        if (boundary_point_set_types[index_boundary - 1] != PointSetType_t::PointList) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", boundary " << index_boundary << " has a point set type that isn't PointList. For now only PointList point set types are supported. Exiting." << std::endl;
+            exit(31);
+        }
+
+        cg_boco_gridlocation_read(index_file, index_base, index_zone, index_boundary, &boundary_grid_locations[index_boundary - 1]);
+
+        if (boundary_grid_locations[index_boundary - 1] != GridLocation_t::EdgeCenter) {
+            std::cerr << "Error: CGNS mesh, base " << index_base << ", zone " << index_zone << ", boundary " << index_boundary << " has a grid location that isn't EdgeCenter. For now only EdgeCenter grid locations are supported. Exiting." << std::endl;
+            exit(32);
+        }
+    }
+
+    std::vector<std::vector<int>> boundary_elements(n_boundaries);
+    std::vector<std::vector<int>> boundary_normals(n_boundaries);
+    for (int index_boundary = 1; index_boundary <= n_boundaries; ++index_boundary) {
+        boundary_elements[index_boundary - 1] = std::vector<int>(boundary_sizes[index_boundary - 1]);
+        boundary_normals[index_boundary - 1] = std::vector<int>(boundary_normal_list_sizes[index_boundary - 1]);
+        cg_boco_read(index_file, index_base, index_zone, index_boundary, boundary_elements[index_boundary - 1].data(), boundary_normals[index_boundary - 1].data());
+    }
+
+    cg_close(index_file);
+
+
+
     return 0;
 }
