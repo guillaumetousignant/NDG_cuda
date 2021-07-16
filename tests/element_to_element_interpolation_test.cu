c52724bf97e6afdee98a3f53f8a8c4f6fc866a77
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-07-16 13:45:23 -0400

    added two p-adaptivity tests

--- tests/element_to_element_interpolation_test.cu
+++ tests/element_to_element_interpolation_test.cu
@@ -66,7 +66,52 @@ auto element_to_element_projection_init(int N, size_t n_elements, SEM::Entities:
     }
 }
 
-TEST_CASE("Element to element projection test", "Projects the solution from one element to another and checks the values match.") {   
+__global__
+auto element_to_element_projection_init_2(int N, int N_high, size_t n_elements, SEM::Entities::Element2D_t* elements, SEM::Entities::Element2D_t* elements_high, const deviceFloat* polynomial_nodes, const deviceFloat* barycentric_weights, deviceFloat* p, deviceFloat* u, deviceFloat* v) -> void {
+    const int index = blockIdx.x * blockDim.x + threadIdx.x;
+    const int stride = blockDim.x * gridDim.x;
+
+    for (size_t element_index = index; element_index < n_elements; element_index += stride) {
+        std::array<SEM::Entities::cuda_vector<size_t>, 4> element_faces {SEM::Entities::cuda_vector<size_t>(1),
+            SEM::Entities::cuda_vector<size_t>(1),
+            SEM::Entities::cuda_vector<size_t>(1),
+            SEM::Entities::cuda_vector<size_t>(1)};
+
+        element_faces[0][0] = element_index;
+        element_faces[1][0] = element_index;
+        element_faces[2][0] = element_index;
+        element_faces[3][0] = element_index;
+
+        elements[element_index] = SEM::Entities::Element2D_t(N, 0, element_faces, std::array<size_t, 4>{4 * element_index, 4 * element_index + 1, 4 * element_index + 2, 4 * element_index + 3});
+        elements_high[element_index] = SEM::Entities::Element2D_t(N_high, 1, element_faces, std::array<size_t, 4>{4 * element_index, 4 * element_index + 1, 4 * element_index + 2, 4 * element_index + 3});
+        
+        SEM::Entities::Element2D_t& element = elements[element_index];
+        SEM::Entities::Element2D_t& element_high = elements_high[element_index];
+        const size_t offset_1D = element.N_ * (element.N_ + 1) /2;
+
+        for (int i = 0; i <= element.N_; ++i) {
+            for (int j = 0; j <= element.N_; ++j) {
+                const Vec2<deviceFloat> coordinates {polynomial_nodes[offset_1D + i], polynomial_nodes[offset_1D + j]};
+                const Vec2<deviceFloat> global_coordinates = SEM::quad_map(coordinates, points[element_index]);
+
+                element.p_[i * (element.N_ + 1) + j] = std::sin(global_coordinates[0]) * std::cos(global_coordinates[1]);
+                element.u_[i * (element.N_ + 1) + j] = global_coordinates[0];
+                element.v_[i * (element.N_ + 1) + j] = global_coordinates[1];
+            }
+        }
+
+        element_high.interpolate_from(element, polynomial_nodes, barycentric_weights);
+
+        const size_t offset_results = element_index * (N_high + 1) * (N_high + 1);
+        for (int i = 0; i < (N_high + 1) * (N_high + 1); ++i) {
+            p[offset_results + i] = element_high.p_[i];
+            u[offset_results + i] = element_high.u_[i];
+            v[offset_results + i] = element_high.v_[i];
+        }
+    }
+}
+
+TEST_CASE("Element to smaller element projection test", "Projects the solution from one element to a smaller one, as in h-adaptivity and checks the values match.") {   
     const int N_max = 16;
     const int N_test = 16;
     const size_t N_interpolation_points = N_max;
@@ -138,6 +183,87 @@ TEST_CASE("Element to element projection test", "Projects the solution from one 
 
     elements.clear(stream);
     elements_small.clear(stream);
+    p.clear(stream);
+    u.clear(stream);
+    v.clear(stream);
+
+    cudaStreamDestroy(stream);
+}
+
+TEST_CASE("Element to higher order element projection test", "Projects the solution from one element to a higher order one, as in p-adaptivity and checks the values match.") {   
+    const int N_max = 16;
+    const int N_test = 16;
+    const int N_test_low = N_test - 2;
+    const size_t N_interpolation_points = N_max;
+    constexpr size_t n_elements = 1;
+    const double max_error = 1e-9;
+
+    REQUIRE(N_test <= N_max);
+    REQUIRE(N_test_low <= N_max);
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream); 
+
+    SEM::Entities::NDG_t<SEM::Polynomials::LegendrePolynomial_t> NDG(N_max, N_interpolation_points, stream);
+    SEM::Entities::device_vector<SEM::Entities::Element2D_t> elements(n_elements, stream);
+    SEM::Entities::device_vector<SEM::Entities::Element2D_t> elements_high(n_elements, stream);
+
+    constexpr int elements_blockSize = 32;
+    constexpr int elements_numBlocks = (n_elements + elements_blockSize - 1) / elements_blockSize;
+
+    SEM::Entities::device_vector<deviceFloat> p(n_elements * (N_test + 1) * (N_test + 1));
+    SEM::Entities::device_vector<deviceFloat> u(n_elements * (N_test + 1) * (N_test + 1));
+    SEM::Entities::device_vector<deviceFloat> v(n_elements * (N_test + 1) * (N_test + 1));
+
+    element_to_element_projection_init_2<<<elements_blockSize, elements_numBlocks, 0, stream>>>(N_test_low, N_test, n_elements, elements.data(), elements_high.data(), NDG.nodes_.data(), NDG.barycentric_weights_.data(), p.data(), u.data(), v.data());
+
+    std::vector<deviceFloat> polynomial_nodes_host(NDG.nodes_.size());
+    NDG.nodes_.copy_to(polynomial_nodes_host, stream);
+
+    std::vector<deviceFloat> p_host(n_elements * (N_test + 1) * (N_test + 1));
+    std::vector<deviceFloat> u_host(n_elements * (N_test + 1) * (N_test + 1));
+    std::vector<deviceFloat> v_host(n_elements * (N_test + 1) * (N_test + 1));
+
+    p.copy_to(p_host, stream);
+    u.copy_to(u_host, stream);
+    v.copy_to(v_host, stream);
+
+    std::vector<std::vector<deviceFloat>> p_expected(n_elements);
+    std::vector<std::vector<deviceFloat>> u_expected(n_elements);
+    std::vector<std::vector<deviceFloat>> v_expected(n_elements);
+
+    for (size_t element_index = 0; element_index < n_elements; ++element_index) {
+        p_expected[element_index] = std::vector<deviceFloat>((N_test + 1) * (N_test + 1));
+        u_expected[element_index] = std::vector<deviceFloat>((N_test + 1) * (N_test + 1));
+        v_expected[element_index] = std::vector<deviceFloat>((N_test + 1) * (N_test + 1));
+        const size_t offset_1D = N_test * (N_test + 1) /2;
+
+        for (size_t i = 0; i <= N_test; ++i) {
+            for (size_t j = 0; j <= N_test; ++j) {
+                const Vec2<deviceFloat> coordinates {polynomial_nodes_host[offset_1D + i], polynomial_nodes_host[offset_1D + j]};
+                const Vec2<deviceFloat> global_coordinates = SEM::quad_map(coordinates, points_small[element_index]);
+
+                p_expected[element_index][i * (N_test + 1) + j] = std::sin(global_coordinates[0]) * std::cos(global_coordinates[1]);
+                u_expected[element_index][i * (N_test + 1) + j] = global_coordinates[0];
+                v_expected[element_index][i * (N_test + 1) + j] = global_coordinates[1];
+            }
+        }
+    }
+
+    for (size_t element_index = 0; element_index < n_elements; ++element_index) {
+        const size_t offset_results = element_index * (N_test + 1) * (N_test + 1);
+
+        for (size_t i = 0; i <= N_test; ++i) {
+            for (size_t j = 0; j <= N_test; ++j) {
+                REQUIRE(std::abs(p_expected[element_index][i * (N_test + 1) + j] - p_host[offset_results + i * (N_test + 1) + j]) < max_error);
+                REQUIRE(std::abs(u_expected[element_index][i * (N_test + 1) + j] - u_host[offset_results + i * (N_test + 1) + j]) < max_error);
+                REQUIRE(std::abs(v_expected[element_index][i * (N_test + 1) + j] - v_host[offset_results + i * (N_test + 1) + j]) < max_error);
+            }
+        }
+    }
+
+    elements.clear(stream);
+    elements_high.clear(stream);
     p.clear(stream);
     u.clear(stream);
     v.clear(stream);
--- tests/face_to_element_projection_test.cu
+++ tests/face_to_element_projection_test.cu
@@ -13,7 +13,7 @@
 using SEM::Entities::Vec2;
 
 __global__
-auto face_to_element_projection_init(int N, size_t n_faces, SEM::Entities::Element2D_t* elements, SEM::Entities::Face2D_t* faces, const Vec2<deviceFloat>* nodes, const deviceFloat* polynomial_nodes) -> void {
+auto face_to_element_projection_init(int N_test_elements, int N_test_faces, size_t n_faces, SEM::Entities::Element2D_t* elements, SEM::Entities::Face2D_t* faces, const Vec2<deviceFloat>* nodes, const deviceFloat* polynomial_nodes) -> void {
     const int index = blockIdx.x * blockDim.x + threadIdx.x;
     const int stride = blockDim.x * gridDim.x;
 
@@ -28,10 +28,10 @@ auto face_to_element_projection_init(int N, size_t n_faces, SEM::Entities::Eleme
         element_faces[2][0] = i;
         element_faces[3][0] = i;
 
-        elements[2 * i]     = SEM::Entities::Element2D_t(N, 0, element_faces, std::array<size_t, 4>{6 * i, 6 * i + 1, 6 * i + 2, 6 * i + 3});
-        elements[2 * i + 1] = SEM::Entities::Element2D_t(N, 0, element_faces, std::array<size_t, 4>{6 * i + 4, 6 * i + 5, 6 * i + 1, 6 * i});
+        elements[2 * i]     = SEM::Entities::Element2D_t(N_test_elements, 0, element_faces, std::array<size_t, 4>{6 * i, 6 * i + 1, 6 * i + 2, 6 * i + 3});
+        elements[2 * i + 1] = SEM::Entities::Element2D_t(N_test_elements, 0, element_faces, std::array<size_t, 4>{6 * i + 4, 6 * i + 5, 6 * i + 1, 6 * i});
 
-        faces[i] = SEM::Entities::Face2D_t(N, std::array<size_t, 2>{6 * i, 6 * i + 1}, std::array<size_t, 2>{2 * i, 2 * i + 1}, std::array<size_t, 2>{0, 2});
+        faces[i] = SEM::Entities::Face2D_t(N_test_faces, std::array<size_t, 2>{6 * i, 6 * i + 1}, std::array<size_t, 2>{2 * i, 2 * i + 1}, std::array<size_t, 2>{0, 2});
 
         faces[i].offset_     = {0.0, 0.0};
         faces[i].scale_      = {1.0, 1.0};
@@ -159,13 +159,15 @@ auto retrieve_element_projected_solution_2(int N, size_t n_faces, const SEM::Ent
 
 TEST_CASE("Face to element projection test", "Projects the face flux solution of a face to its elements and checks the values match.") {   
     const int N_max = 16;
-    const int N_test = 16;
+    const int N_test_faces = 16;
+    const int N_test_elements = N_test_faces;
     const size_t N_interpolation_points = N_max;
     constexpr size_t n_faces = 1;
     constexpr size_t n_elements = 2 * n_faces;
     const double max_error = 1e-9;
 
-    REQUIRE(N_test <= N_max);
+    REQUIRE(N_test_elements <= N_max);
+    REQUIRE(N_test_faces <= N_max);
 
     cudaStream_t stream;
     cudaStreamCreate(&stream); 
@@ -187,7 +189,7 @@ TEST_CASE("Face to element projection test", "Projects the face flux solution of
     constexpr int elements_blockSize = 32;
     constexpr int elements_numBlocks = (n_elements + elements_blockSize - 1) / elements_blockSize;
 
-    face_to_element_projection_init<<<faces_blockSize, faces_numBlocks, 0, stream>>>(N_test, n_faces, elements.data(), faces.data(), nodes.data(), NDG.nodes_.data());
+    face_to_element_projection_init<<<faces_blockSize, faces_numBlocks, 0, stream>>>(N_test_elements, N_test_faces, n_faces, elements.data(), faces.data(), nodes.data(), NDG.nodes_.data());
 
     SEM::Meshes::compute_element_geometry<<<elements_blockSize, elements_numBlocks, 0, stream>>>(n_elements, elements.data(), nodes.data(), NDG.nodes_.data());
 
@@ -201,13 +203,13 @@ TEST_CASE("Face to element projection test", "Projects the face flux solution of
     std::vector<std::array<std::vector<deviceFloat>, 2>> u_expected(n_faces);
     std::vector<std::array<std::vector<deviceFloat>, 2>> v_expected(n_faces);
     
-    constexpr size_t offset_1D = N_test * (N_test + 1) /2;
+    constexpr size_t offset_1D = N_test_elements * (N_test_elements + 1) /2;
     for (size_t i = 0; i < n_faces; ++i) {
-        p_expected[i] = {std::vector<deviceFloat>(N_test + 1), std::vector<deviceFloat>(N_test + 1)};
-        u_expected[i] = {std::vector<deviceFloat>(N_test + 1), std::vector<deviceFloat>(N_test + 1)};
-        v_expected[i] = {std::vector<deviceFloat>(N_test + 1), std::vector<deviceFloat>(N_test + 1)};
+        p_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
+        u_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
+        v_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
     
-        for (int j = 0; j <= N_test; ++j) {
+        for (int j = 0; j <= N_test_elements; ++j) {
             const deviceFloat interp = (polynomial_nodes_host[offset_1D + j] + 1)/2;
             const Vec2<deviceFloat> global_coordinates = host_nodes[6 * i + 1] * interp + host_nodes[6 * i] * (1 - interp);
             
@@ -215,37 +217,141 @@ TEST_CASE("Face to element projection test", "Projects the face flux solution of
             u_expected[i][0][j] = global_coordinates.y() * global_coordinates.y();
             v_expected[i][0][j] = global_coordinates.y();
 
-            p_expected[i][1][N_test - j] = -std::cos(global_coordinates.y());
-            u_expected[i][1][N_test - j] = -global_coordinates.y() * global_coordinates.y();
-            v_expected[i][1][N_test - j] = -global_coordinates.y();
+            p_expected[i][1][N_test_elements - j] = -std::cos(global_coordinates.y());
+            u_expected[i][1][N_test_elements - j] = -global_coordinates.y() * global_coordinates.y();
+            v_expected[i][1][N_test_elements - j] = -global_coordinates.y();
         }
     }
 
-    SEM::Entities::device_vector<deviceFloat> p(n_faces * 2 * (N_test + 1), stream);
-    SEM::Entities::device_vector<deviceFloat> u(n_faces * 2 * (N_test + 1), stream);
-    SEM::Entities::device_vector<deviceFloat> v(n_faces * 2 * (N_test + 1), stream);
+    SEM::Entities::device_vector<deviceFloat> p(n_elements * (N_test_elements + 1), stream);
+    SEM::Entities::device_vector<deviceFloat> u(n_elements * (N_test_elements + 1), stream);
+    SEM::Entities::device_vector<deviceFloat> v(n_elements * (N_test_elements + 1), stream);
 
-    retrieve_element_projected_solution<<<elements_numBlocks, elements_blockSize, 0, stream>>>(N_test, n_faces, elements.data(), faces.data(), p.data(), u.data(), v.data());
+    retrieve_element_projected_solution<<<elements_numBlocks, elements_blockSize, 0, stream>>>(N_test_elements, n_faces, elements.data(), faces.data(), p.data(), u.data(), v.data());
 
-    std::vector<deviceFloat> p_host(n_faces * 2 * (N_test + 1));
-    std::vector<deviceFloat> u_host(n_faces * 2 * (N_test + 1));
-    std::vector<deviceFloat> v_host(n_faces * 2 * (N_test + 1));
+    std::vector<deviceFloat> p_host(n_elements * (N_test_elements + 1));
+    std::vector<deviceFloat> u_host(n_elements * (N_test_elements + 1));
+    std::vector<deviceFloat> v_host(n_elements * (N_test_elements + 1));
 
     p.copy_to(p_host, stream);
     u.copy_to(u_host, stream);
     v.copy_to(v_host, stream);
 
     for (size_t i = 0; i < n_faces; ++i) {
-        const size_t offset_results = 2 * i * (N_test + 1);
+        const size_t offset_results = 2 * i * (N_test_elements + 1);
 
-        for (int j = 0; j <= N_test; ++j) {
+        for (int j = 0; j <= N_test_elements; ++j) {
             REQUIRE(std::abs(p_expected[i][0][j] - p_host[offset_results + j]) < max_error);
             REQUIRE(std::abs(u_expected[i][0][j] - u_host[offset_results + j]) < max_error);
             REQUIRE(std::abs(v_expected[i][0][j] - v_host[offset_results + j]) < max_error);
 
-            REQUIRE(std::abs(p_expected[i][1][j] - p_host[offset_results + N_test + 1 + j]) < max_error);
-            REQUIRE(std::abs(u_expected[i][1][j] - u_host[offset_results + N_test + 1 + j]) < max_error);
-            REQUIRE(std::abs(v_expected[i][1][j] - v_host[offset_results + N_test + 1 + j]) < max_error);
+            REQUIRE(std::abs(p_expected[i][1][j] - p_host[offset_results + N_test_elements + 1 + j]) < max_error);
+            REQUIRE(std::abs(u_expected[i][1][j] - u_host[offset_results + N_test_elements + 1 + j]) < max_error);
+            REQUIRE(std::abs(v_expected[i][1][j] - v_host[offset_results + N_test_elements + 1 + j]) < max_error);
+        }
+    }
+
+    elements.clear(stream);
+    faces.clear(stream);
+    nodes.clear(stream);
+    p.clear(stream);
+    u.clear(stream);
+    v.clear(stream);
+
+    cudaStreamDestroy(stream);
+}
+
+TEST_CASE("Face to lower order element projection test", "Projects the face flux solution of a face to its lower order elements and checks the values match.") {   
+    const int N_max = 16;
+    const int N_test_faces = 16;
+    const int N_test_elements = N_test_faces - 2;
+    const size_t N_interpolation_points = N_max;
+    constexpr size_t n_faces = 1;
+    constexpr size_t n_elements = 2 * n_faces;
+    const double max_error = 1e-9;
+
+    REQUIRE(N_test_elements <= N_max);
+    REQUIRE(N_test_faces <= N_max);
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream); 
+
+    const std::vector<Vec2<deviceFloat>> host_nodes {Vec2<deviceFloat>{0, -1},
+                                                     Vec2<deviceFloat>{0, 1},
+                                                     Vec2<deviceFloat>{-1, 1},
+                                                     Vec2<deviceFloat>{-1, -1},
+                                                     Vec2<deviceFloat>{1, -1},
+                                                     Vec2<deviceFloat>{1, 1}};
+
+    SEM::Entities::NDG_t<SEM::Polynomials::LegendrePolynomial_t> NDG(N_max, N_interpolation_points, stream);
+    SEM::Entities::device_vector<SEM::Entities::Element2D_t> elements(n_elements, stream);
+    SEM::Entities::device_vector<SEM::Entities::Face2D_t> faces(n_faces, stream);
+    SEM::Entities::device_vector<Vec2<deviceFloat>> nodes(host_nodes, stream);
+
+    constexpr int faces_blockSize = 32;
+    constexpr int faces_numBlocks = (n_faces + faces_blockSize - 1) / faces_blockSize;
+    constexpr int elements_blockSize = 32;
+    constexpr int elements_numBlocks = (n_elements + elements_blockSize - 1) / elements_blockSize;
+
+    face_to_element_projection_init<<<faces_blockSize, faces_numBlocks, 0, stream>>>(N_test_elements, N_test_faces, n_faces, elements.data(), faces.data(), nodes.data(), NDG.nodes_.data());
+
+    SEM::Meshes::compute_element_geometry<<<elements_blockSize, elements_numBlocks, 0, stream>>>(n_elements, elements.data(), nodes.data(), NDG.nodes_.data());
+
+    SEM::Meshes::project_to_elements<<<elements_blockSize, elements_numBlocks, 0, stream>>>(n_elements, faces.data(), elements.data(), NDG.nodes_.data(), NDG.weights_.data(), NDG.barycentric_weights_.data());
+    
+    std::vector<deviceFloat> polynomial_nodes_host(NDG.nodes_.size());
+
+    NDG.nodes_.copy_to(polynomial_nodes_host, stream);
+
+    std::vector<std::array<std::vector<deviceFloat>, 2>> p_expected(n_faces);
+    std::vector<std::array<std::vector<deviceFloat>, 2>> u_expected(n_faces);
+    std::vector<std::array<std::vector<deviceFloat>, 2>> v_expected(n_faces);
+    
+    constexpr size_t offset_1D = N_test_elements * (N_test_elements + 1) /2;
+    for (size_t i = 0; i < n_faces; ++i) {
+        p_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
+        u_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
+        v_expected[i] = {std::vector<deviceFloat>(N_test_elements + 1), std::vector<deviceFloat>(N_test_elements + 1)};
+    
+        for (int j = 0; j <= N_test_elements; ++j) {
+            const deviceFloat interp = (polynomial_nodes_host[offset_1D + j] + 1)/2;
+            const Vec2<deviceFloat> global_coordinates = host_nodes[6 * i + 1] * interp + host_nodes[6 * i] * (1 - interp);
+            
+            p_expected[i][0][j] = std::cos(global_coordinates.y());
+            u_expected[i][0][j] = global_coordinates.y() * global_coordinates.y();
+            v_expected[i][0][j] = global_coordinates.y();
+
+            p_expected[i][1][N_test_elements - j] = -std::cos(global_coordinates.y());
+            u_expected[i][1][N_test_elements - j] = -global_coordinates.y() * global_coordinates.y();
+            v_expected[i][1][N_test_elements - j] = -global_coordinates.y();
+        }
+    }
+
+    SEM::Entities::device_vector<deviceFloat> p(n_elements * (N_test_elements + 1), stream);
+    SEM::Entities::device_vector<deviceFloat> u(n_elements * (N_test_elements + 1), stream);
+    SEM::Entities::device_vector<deviceFloat> v(n_elements * (N_test_elements + 1), stream);
+
+    retrieve_element_projected_solution<<<elements_numBlocks, elements_blockSize, 0, stream>>>(N_test_elements, n_faces, elements.data(), faces.data(), p.data(), u.data(), v.data());
+
+    std::vector<deviceFloat> p_host(n_elements * (N_test_elements + 1));
+    std::vector<deviceFloat> u_host(n_elements * (N_test_elements + 1));
+    std::vector<deviceFloat> v_host(n_elements * (N_test_elements + 1));
+
+    p.copy_to(p_host, stream);
+    u.copy_to(u_host, stream);
+    v.copy_to(v_host, stream);
+
+    for (size_t i = 0; i < n_faces; ++i) {
+        const size_t offset_results = 2 * i * (N_test_elements + 1);
+
+        for (int j = 0; j <= N_test_elements; ++j) {
+            REQUIRE(std::abs(p_expected[i][0][j] - p_host[offset_results + j]) < max_error);
+            REQUIRE(std::abs(u_expected[i][0][j] - u_host[offset_results + j]) < max_error);
+            REQUIRE(std::abs(v_expected[i][0][j] - v_host[offset_results + j]) < max_error);
+
+            REQUIRE(std::abs(p_expected[i][1][j] - p_host[offset_results + N_test_elements + 1 + j]) < max_error);
+            REQUIRE(std::abs(u_expected[i][1][j] - u_host[offset_results + N_test_elements + 1 + j]) < max_error);
+            REQUIRE(std::abs(v_expected[i][1][j] - v_host[offset_results + N_test_elements + 1 + j]) < max_error);
         }
     }
 
