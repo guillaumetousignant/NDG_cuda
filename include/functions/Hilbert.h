b05ee2fcda1303e0a70817ab26012c746f47d829
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-08-13 09:12:35 -0400

    added Hilbert status enum

--- include/functions/Hilbert.h
+++ include/functions/Hilbert.h
@@ -4,6 +4,14 @@
 #include <array>
 
 namespace SEM { namespace Hilbert {
+    /**
+     * @brief Describes the geometrical arrangement of a cell, one of four possible values.
+     * 
+     * This is necessary to the table-driven algorithm for the Hilbert curve, where the order
+     * and status oh the next level cells is determined by its parent's status.
+     */
+    enum Status {H, A, R, B};
+
     //convert (x,y) to d
     auto xy2d(int n, std::array<int, 2> xy) -> int;
 
