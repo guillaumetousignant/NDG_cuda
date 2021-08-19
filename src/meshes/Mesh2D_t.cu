d1b98a76758c85da0ef212a5201c369ad9950722
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-08-19 14:39:11 -0400

    how many elements we send

--- src/meshes/Mesh2D_t.cu
+++ src/meshes/Mesh2D_t.cu
@@ -1845,6 +1845,10 @@ auto SEM::Meshes::Mesh2D_t::load_balance() -> void {
     const size_t global_element_offset_end_new = std::min(global_element_offset_new + n_elements_per_process_new - 1, n_elements_global_new - 1); // CHECK does this work for empty procs?
     const size_t n_elements_new = (global_rank == global_size - 1) ? n_elements_global_new - n_elements_global_new * (global_size - 1) : n_elements_per_process_new; // CHECK does this work for empty procs?
 
+    const size_t n_elements_send_left = (global_element_offset_new > global_element_offset_current) ? global_element_offset_new - global_element_offset_current : 0;
+    const size_t n_elements_recv_left = (global_element_offset_current > global_element_offset_new) ? global_element_offset_current - global_element_offset_new : 0;
+    const size_t n_elements_send_right = (global_element_offset_end_current > global_element_offset_end_new) ? global_element_offset_end_current - global_element_offset_end_new : 0;
+    const size_t n_elements_recv_right = (global_element_offset_end_new > global_element_offset_end_current) ? global_element_offset_end_new - global_element_offset_end_current : 0;
 
 
 
