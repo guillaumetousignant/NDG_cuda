271ec4dacec42f0bf36cd794b46c9ebeb15ff6d7
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-09-30 13:19:40 -0400

    We start the wave outside the domain

--- include/helpers/constants.h
+++ include/helpers/constants.h
@@ -13,7 +13,7 @@ namespace SEM {
      */
     namespace Constants {
         constexpr deviceFloat c = 1;
-        constexpr SEM::Entities::Vec2<deviceFloat> xy0 {0.2, 0.2};
+        constexpr SEM::Entities::Vec2<deviceFloat> xy0 {-0.2, -0.2};
         constexpr SEM::Entities::Vec2<deviceFloat> k {0.707106781186548, 0.707106781186548}; // √2/2, √2/2
         constexpr deviceFloat d = 0.120112240878645; // 0.2/(2 * √ln(2))
         constexpr int n_points_least_squares_max = 4; // Don't use more than the last n points from the spectrum to estimate error
