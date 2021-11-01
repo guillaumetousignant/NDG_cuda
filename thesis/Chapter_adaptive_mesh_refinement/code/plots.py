046ddc83c713a70989afb449c44fdfc411ea3848
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-11-01 15:18:34 -0400

    more plots

--- thesis/Chapter_adaptive_mesh_refinement/code/plots.py
+++ thesis/Chapter_adaptive_mesh_refinement/code/plots.py
@@ -16,36 +16,79 @@ nodes_6 = sem.LegendreGaussNodesAndWeights(N[1])[0]
 
 nodes = [nodes_4, nodes_6]
 
-points_fig, points_ax = plt.subplots(1, 1)
-points_ax.set_xlim(-1.1, 5.1)
-points_ax.set_ylim(-1.1, 1.1)
-points_ax.set_aspect(1)
-points_ax.axis('off')
+p_fig, p_ax = plt.subplots(1, 1)
+p_ax.set_xlim(-1.1, 5.1)
+p_ax.set_ylim(-1.1, 1.1)
+p_ax.set_aspect(1)
+p_ax.axis('off')
 
-points_ax.plot([-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1], label="N = 4 outline", color=outline_colour, linewidth=2)
+p_ax.plot([-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1], color=outline_colour, linewidth=2)
 
 for i in range(N[0] + 1):
-    points_ax.plot([nodes[0][i], nodes[0][i]], [-1, 1], color=lines_colour, linewidth=1)
-    points_ax.plot([-1, 1], [nodes[0][i], nodes[0][i]], color=lines_colour, linewidth=1)
+    p_ax.plot([nodes[0][i], nodes[0][i]], [-1, 1], color=lines_colour, linewidth=1)
+    p_ax.plot([-1, 1], [nodes[0][i], nodes[0][i]], color=lines_colour, linewidth=1)
 
 for i in range(N[0] + 1):
     for j in range(N[0] + 1):
-        points_ax.plot(nodes[0][i], nodes[0][j], color=points_colour, linewidth=5, marker="o", markersize=4)
+        p_ax.plot(nodes[0][i], nodes[0][j], color=points_colour, linewidth=6, marker="o", markersize=4)
     
-points_ax.plot([3, 5, 5, 3, 3], [-1, -1, 1, 1, -1], label="N = 6 outline", color=outline_colour, linewidth=2)
+p_ax.plot([3, 5, 5, 3, 3], [-1, -1, 1, 1, -1], color=outline_colour, linewidth=2)
 
 for i in range(N[1] + 1):
-    points_ax.plot([nodes[1][i] + 4, nodes[1][i] + 4], [-1, 1], color=lines_colour, linewidth=1)
-    points_ax.plot([3, 5], [nodes[1][i], nodes[1][i]], color=lines_colour, linewidth=1)
+    p_ax.plot([nodes[1][i] + 4, nodes[1][i] + 4], [-1, 1], color=lines_colour, linewidth=1)
+    p_ax.plot([3, 5], [nodes[1][i], nodes[1][i]], color=lines_colour, linewidth=1)
 
 for i in range(N[1] + 1):
     for j in range(N[1] + 1):
-        points_ax.plot(nodes[1][i] + 4, nodes[1][j], color=points_colour, linewidth=5, marker="o", markersize=4)
+        p_ax.plot(nodes[1][i] + 4, nodes[1][j], color=points_colour, linewidth=6, marker="o", markersize=4)
 
-points_ax.arrow(1.5, 0, 1, 0, length_includes_head=False, width=0.05, head_length=0.1, color=outline_colour)
+p_ax.arrow(1.5, 0, 1, 0, length_includes_head=False, width=0.05, head_length=0.1, color=outline_colour)
 
-points_fig.savefig(save_path / f"collocation_points_N{N[0]}_N{N[1]}.svg", format='svg', transparent=True)
-points_fig.savefig(save_path / f"collocation_points_N{N[0]}_N{N[1]}.png", format='png', transparent=True)
+p_fig.savefig(save_path / f"p-adaptivity_N{N[0]}_N{N[1]}.svg", format='svg', transparent=True)
+p_fig.savefig(save_path / f"p-adaptivity_N{N[0]}_N{N[1]}.png", format='png', transparent=True)
+
+h_fig, h_ax = plt.subplots(1, 1)
+h_ax.set_xlim(-1.1, 5.1)
+h_ax.set_ylim(-1.1, 1.1)
+h_ax.set_aspect(1)
+h_ax.axis('off')
+
+h_ax.plot([-1, 1, 1, -1, -1], [-1, -1, 1, 1, -1], color=outline_colour, linewidth=2)
+
+for i in range(N[0] + 1):
+    h_ax.plot([nodes[0][i], nodes[0][i]], [-1, 1], color=lines_colour, linewidth=1)
+    h_ax.plot([-1, 1], [nodes[0][i], nodes[0][i]], color=lines_colour, linewidth=1)
+
+for i in range(N[0] + 1):
+    for j in range(N[0] + 1):
+        h_ax.plot(nodes[0][i], nodes[0][j], color=points_colour, linewidth=4, marker="o", markersize=4)
+
+h_ax.plot([3, 4, 4, 3, 3], [-1, -1, 0, 0, -1], color=outline_colour, linewidth=2)
+h_ax.plot([4, 5, 5, 4, 4], [-1, -1, 0, 0, -1], color=outline_colour, linewidth=2)
+h_ax.plot([4, 5, 5, 4, 4], [0, 0, 1, 1, 0], color=outline_colour, linewidth=2)
+h_ax.plot([3, 4, 4, 3, 3], [0, 0, 1, 1, 0], color=outline_colour, linewidth=2)
+
+for i in range(N[0] + 1):
+    h_ax.plot([nodes[0][i]/2 + 3.5, nodes[0][i]/2 + 3.5], [-1, 0], color=lines_colour, linewidth=1)
+    h_ax.plot([nodes[0][i]/2 + 4.5, nodes[0][i]/2 + 4.5], [-1, 0], color=lines_colour, linewidth=1)
+    h_ax.plot([nodes[0][i]/2 + 3.5, nodes[0][i]/2 + 3.5], [0, 1], color=lines_colour, linewidth=1)
+    h_ax.plot([nodes[0][i]/2 + 4.5, nodes[0][i]/2 + 4.5], [0, 1], color=lines_colour, linewidth=1)
+    h_ax.plot([3, 4], [nodes[0][i]/2 - 0.5, nodes[0][i]/2 - 0.5], color=lines_colour, linewidth=1)
+    h_ax.plot([4, 5], [nodes[0][i]/2 - 0.5, nodes[0][i]/2 - 0.5], color=lines_colour, linewidth=1)
+    h_ax.plot([4, 5], [nodes[0][i]/2 + 0.5, nodes[0][i]/2 + 0.5], color=lines_colour, linewidth=1)
+    h_ax.plot([3, 4], [nodes[0][i]/2 + 0.5, nodes[0][i]/2 + 0.5], color=lines_colour, linewidth=1)
+
+for i in range(N[0] + 1):
+    for j in range(N[0] + 1):
+        h_ax.plot(nodes[0][i]/2 + 3.5, nodes[0][j]/2 - 0.5, color=points_colour, linewidth=4, marker="o", markersize=4)
+        h_ax.plot(nodes[0][i]/2 + 4.5, nodes[0][j]/2 - 0.5, color=points_colour, linewidth=4, marker="o", markersize=4)
+        h_ax.plot(nodes[0][i]/2 + 4.5, nodes[0][j]/2 + 0.5, color=points_colour, linewidth=4, marker="o", markersize=4)
+        h_ax.plot(nodes[0][i]/2 + 3.5, nodes[0][j]/2 + 0.5, color=points_colour, linewidth=4, marker="o", markersize=4)
+
+h_ax.arrow(1.5, 0, 1, 0, length_includes_head=False, width=0.05, head_length=0.1, color=outline_colour)
+
+h_fig.savefig(save_path / f"h-adaptivity_N{N[0]}.svg", format='svg', transparent=True)
+h_fig.savefig(save_path / f"h-adaptivity_N{N[0]}.png", format='png', transparent=True)
 
 plt.show()
     
