17cbad135a2f73d1bc850aff2819ad826b1615f4
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-11-16 11:40:43 -0500

    div is not defined in device code 3: the return

--- src/meshes/Mesh2D_t.cu
+++ src/meshes/Mesh2D_t.cu
@@ -7759,7 +7759,8 @@ auto SEM::Device::Meshes::add_new_received_neighbour_nodes(size_t n_received_nei
                 received_neighbour_nodes_indices[i] = new_received_index;
             }
             else {
-                const auto [received_block, received_thread] = std::div(static_cast<long long>(received_neighbour_node_received_indices[i] - n_received_nodes), blockDim.x);
+                const int received_block = (received_neighbour_node_received_indices[i] - n_received_nodes)/blockDim.x;
+                const int received_thread = (received_neighbour_node_received_indices[i] - n_received_nodes)%blockDim.x;
 
                 size_t new_received_index = n_nodes + received_neighbour_nodes_block_offsets[received_block];
                 for (size_t j = received_neighbour_node_received_indices[i] - n_received_nodes - received_thread; j < received_neighbour_node_received_indices[i] - n_received_nodes; ++j) {
