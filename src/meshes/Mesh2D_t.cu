1203e8c558b904243c7834c5cb640e728cdcd565
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-11-22 18:55:48 -0500

    debug stuff

--- src/meshes/Mesh2D_t.cu
+++ src/meshes/Mesh2D_t.cu
@@ -8114,6 +8114,7 @@ auto SEM::Device::Meshes::find_faces_to_delete(size_t n_faces, size_t n_domain_e
         };
 
         faces_to_delete[i] = elements_leaving[0] && elements_leaving[1] || elements_leaving[0] && boundary_elements[1] || elements_leaving[1] && boundary_elements[0];
+        printf("Face %llu has delete %d\n", i, faces_to_delete[i]);
     }
 }
 
