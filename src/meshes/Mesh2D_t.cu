37f46d70d238574bbad0203376074deddd37122c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-11-25 19:31:35 -0500

    removed some debug stuff

--- src/meshes/Mesh2D_t.cu
+++ src/meshes/Mesh2D_t.cu
@@ -4224,8 +4224,6 @@ auto SEM::Device::Meshes::project_to_faces(size_t n_faces, Face2D_t* faces, cons
             const size_t offset_1D = face.N_ * (face.N_ + 1) /2;
             const size_t offset_1D_other = element_R.N_ * (element_R.N_ + 1) /2;
 
-            printf("Projecting to face %llu\n", face_index);
-
             for (int i = 0; i <= face.N_; ++i) {
                 const deviceFloat coordinate = (polynomial_nodes[offset_1D + face.N_ - i] + 1) * face.scale_[1] + 2 * face.offset_[1] - 1;
 
@@ -7348,8 +7346,6 @@ auto SEM::Device::Meshes::fill_received_elements_faces(
                     if (neighbours_procs[neighbour_index] < 0) {
                         elements[neighbour_element_index].faces_[0][0] = face_index;
                     }
-
-                    printf("Received element %llu with index %llu created face %llu with: N %d, p: [%p, %p], u: [%p, %p], v: [%p, %p]\n", i, element_index, face_index, faces[face_index].N_, static_cast<void *>(faces[face_index].p_[0].data()), static_cast<void *>(faces[face_index].p_[1].data()), static_cast<void *>(faces[face_index].u_[0].data()), static_cast<void *>(faces[face_index].u_[1].data()), static_cast<void *>(faces[face_index].v_[0].data()), static_cast<void *>(faces[face_index].v_[1].data()));
 
                     ++face_index;
                 }
