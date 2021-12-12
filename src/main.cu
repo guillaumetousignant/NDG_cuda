517f8cfbd24f73294f79495b35d9315e03c52e8b
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-12-12 17:53:21 -0500

    we  have more procs now

--- src/main.cu
+++ src/main.cu
@@ -140,7 +140,7 @@ auto main(int argc, char* argv[]) -> int {
     cudaFree(nullptr); // This makes it debugable, if no cuda call happens before some set time, the memory sanitizer gives up
     int global_dodo_rank = -1;
     MPI_Comm_rank(MPI_COMM_WORLD, &global_dodo_rank);
-    if (global_dodo_rank <= 1) {
+    if (global_dodo_rank <= 4) {
         volatile bool dodo = true;
         while (dodo) {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
