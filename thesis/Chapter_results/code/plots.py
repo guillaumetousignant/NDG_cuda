47e09af2058c21dcb38e2a1b0efb7556900c857c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-12-22 14:40:49 -0500

    added weak scaling

--- thesis/Chapter_results/code/plots.py
+++ thesis/Chapter_results/code/plots.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 
+# Data
+# Strong scaling
 N = np.array([4, 6, 6, 6])
 K = np.array([256 * 256, 256 * 256, 256 * 256, 256 * 256])
 W = np.array([32, 32, 64, 128])
@@ -34,6 +36,24 @@ nodes_beluga_gpu_ideal = nodes_beluga_gpu[:, [0,-1]]
 times_beluga_cpu_ideal = np.stack((min_total_time_beluga_cpu/nodes_beluga_cpu_ideal[:, 0], min_total_time_beluga_cpu/nodes_beluga_cpu_ideal[:, -1]), axis=-1)
 times_beluga_gpu_ideal = np.stack((min_total_time_beluga_gpu/nodes_beluga_gpu_ideal[:, 0], min_total_time_beluga_gpu/nodes_beluga_gpu_ideal[:, -1]), axis=-1)
 
+# Weak scaling
+N_weak = np.array([4])
+K_weak = np.array([64 * 64])
+W_weak = np.array([32])
+
+times_beluga_cpu_weak = np.array([[437.476, 248.753, 300.733, 373.963]])
+nodes_beluga_cpu_weak = np.array([[0.25, 1, 4, 16]])
+min_total_time_beluga_cpu_weak = np.amin(times_beluga_cpu_weak, 1)
+
+times_beluga_gpu_weak = np.array([[496.708, 130.662, 132.527, 129.103]])
+nodes_beluga_gpu_weak = np.array([[0.25, 1, 4, 16]])
+min_total_time_beluga_gpu_weak = np.amin(times_beluga_gpu_weak, 1)
+
+nodes_beluga_cpu_ideal_weak = nodes_beluga_cpu_weak[:, [0,-1]]
+nodes_beluga_gpu_ideal_weak = nodes_beluga_gpu_weak[:, [0,-1]]
+
+# Plots
+# Strong scaling
 save_path = Path(__file__).parent.parent / "media"
 save_path.mkdir(parents=True, exist_ok=True)
 
@@ -64,5 +84,27 @@ for i in range(N.shape[0]):
     ax.legend()
 
     fig.savefig(save_path / f"strong_scaling_N{N[i]}_K{K[i]}_W{W[i]}.svg", format='svg', transparent=True)
+
+# Weak scaling
+for i in range(N_weak.shape[0]):
+    fig = plt.figure(figsize=(4, 4))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel("Number of nodes [-]")
+    ax.set_ylabel("Time [s]")
+    title = f"Weak scaling, N = {N_weak[i]} K = {K_weak[i]}/proc W = {W_weak[i]}"
+    ax.set_title(title)
+    fig.canvas.manager.set_window_title(title)
+    ax.grid()
+
+    ax.loglog(nodes_beluga_cpu_weak[i, :], times_beluga_cpu_weak[i, :], color=cpu_colour, linewidth=data_width, marker=data_shape, markersize=data_size, label="Beluga CPU time")
+    ax.loglog(nodes_beluga_cpu_ideal_weak[i, :], [min_total_time_beluga_cpu_weak[i], min_total_time_beluga_cpu_weak[i]], color=cpu_colour, linewidth=data_width, linestyle=ideal_style, label="Beluga ideal CPU time")
+    
+    ax.loglog(nodes_beluga_gpu_weak[i, :], times_beluga_gpu_weak[i, :], color=gpu_colour, linewidth=data_width, marker=data_shape, markersize=data_size, label="Beluga GPU time")
+    ax.loglog(nodes_beluga_gpu_ideal_weak[i, :], [min_total_time_beluga_gpu_weak[i], min_total_time_beluga_gpu_weak[i]], color=gpu_colour, linewidth=data_width, linestyle=ideal_style, label="Beluga ideal GPU time")
+
+    ax.legend()
+
+    fig.savefig(save_path / f"weak_scaling_N{N_weak[i]}_K{K_weak[i]}_W{W_weak[i]}.svg", format='svg', transparent=True)
+
 
 plt.show()
